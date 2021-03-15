d843ef5296b853c7a6c122248628e1620e51fc40
Santosh Lal Karna <santosh.karna@dryicesolutions.net>
2021-03-15 17:51:57 +0545

    refactored the code

--- src/demo/ProjectDemo.java
+++ src/demo/ProjectDemo.java
@@ -7,17 +7,20 @@ public class ProjectDemo {
     public static void main(String[] args) {
         String cvxList = "207,208,209,210,234";
         String reg = "207|208|209|210";
-        String [] cvx = cvxList.split(",");
+        String[] cvx = cvxList.split(",");
         System.out.println(Arrays.toString(cvx));
+
         int counter = 0;
-        for(int i = 0; i < cvx.length; i++){
-            if(cvx[i].matches(reg)){
+
+        for (int i = 0; i < cvx.length; i++) {
+            if (cvx[i].matches(reg)) {
                 counter++;
             }
         }
-        if(counter == 4){
+
+        if (counter == 4) {
             System.out.println("test pass");
-        }else{
+        } else {
             System.out.println("test fail");
         }
 
